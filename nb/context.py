fb0dbc55324c2a6a039720213712b73537b6cb45
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-06-07 19:28:45 -0400

    move nb code off main article, organize repo

---
+++ nb/context.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+SRC_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(SRC_PATH)
+
+import genre_dag
+
+from genre_dag.util import read_json
+
+DATA_PATH = os.path.join((os.path.dirname(os.getcwd())), "data")
+
+data = read_json(DATA_PATH + "/data.json")
